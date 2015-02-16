59f3a5f54393932bd0136660dc156bb3c8be249a
yangtr <874961524@qq.com>
2015-02-17 00:20:00 +0800

    '对操纵数据库的代码进行封装'

---
+++ include/mysql.func.php
@@ -0,0 +1,78 @@
+<?php 
+if(!defined('IN_CF')) {//没有授权，直接退出
+	exit("Access Defined");
+}
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PWD', 'root');
+define('DB_NAME', 'guest');
+
+
+
+/**
+ * 建立数据库链接
+ * @return void
+ */
+
+function connect() {
+	global $conn;//使这个变量成为全局变量，在函数外部也能访问
+	if (!$conn = mysql_connect(DB_HOST, DB_USER, DB_PWD)){
+		exit('数据库链接错误！');
+	}
+}
+
+/**
+ * 选择数据库
+ * @return void
+ */
+function select_db() {
+	if (!mysql_select_db(DB_NAME)){
+		exit('无法选择指定的数据库！');
+	}
+}
+
+/**
+ * 设置字符集
+ * @return void
+ */
+function set_name() {
+	if (!mysql_query('SET NAMES UTF8')){
+		exit('字符集设置错误！');
+	}
+}
+
+
+/**
+ * 用php写入sql语句
+ * @param string $query
+ */
+function _query($query) {
+	if (!$result = mysql_query($query)){
+		exit('sql执行失败！'.mysql_error());
+	}
+	return $result;
+}
+
+/**
+ * 从结果集中取出一行作为关联数组
+ * @param string $query
+ * @return multitype:
+ */
+function _fetch_query($query) {
+	return mysql_fetch_array(_query($query), MYSQL_ASSOC);
+}
+
+function _close() {
+	if (!mysql_close()){
+		exit('数据库关闭错误!');
+	}
+}
+
+
+
+
+
+
+
+?>

0e6fe8b9a1d4409a2ca1c8dde969e1b99c5bc5ac
yangtr <874961524@qq.com>
2015-02-21 20:39:13 +0800

    博友界面制作底部文本分页

--- blog.php
+++ blog.php
@@ -70,6 +70,29 @@ require ROOT_PATH."include/header.inc.php";//转换硬路径，提高访问速�
 			}?>
 		</ul>
 	</div>
+	<div id="page_text">
+		<ul>
+			<li><?php echo $pagenow; ?>/<?php echo $page_abs; ?>页 |</li>
+			<li>共有<strong><?php echo $page_sum; ?></strong>个会员 |</li>
+			<?php if ($pagenow == 1) {
+						echo '<li>首页 |</li>';
+						echo '<li>上一页 |</li>';
+				  } else {//$_SERVER['SCRIPT_NAME']是php环境变量，会返回当前页面的名称,也可使用本页面自定义的常量SCRIPT
+				  		echo '<li><a href="'.$_SERVER['SCRIPT_NAME'].'?page=1">首页</a> |</li>';
+				  		echo '<li><a href="'.$_SERVER['SCRIPT_NAME'].'?page='.($pagenow-1).'">上一页</a> |</li>';
+				  }
+				 if ($pagenow == $page_abs) {
+						echo '<li>下一页 |</li>';
+						echo '<li>尾页 |</li>';
+						
+				  } else {
+				  		echo '<li><a href="'.$_SERVER['SCRIPT_NAME'].'?page='.($pagenow+1).'">下一页</a> |</li>';
+				  		echo '<li><a href="'.$_SERVER['SCRIPT_NAME'].'?page='.$page_abs.'">尾页</a> |</li>';
+				  		
+				  } 
+			?>
+		</ul>
+	</div>
 </div>
 
 

d70d046c3c9cb40d3d3f0ee12553f7c9c4903857
yangtr <874961524@qq.com>
2015-02-21 18:46:24 +0800

    博友界面page参数做容错处理 允许page 参数为0或者超出页面下标

--- blog.php
+++ blog.php
@@ -3,12 +3,33 @@ header("Content-Type:text/html;charset=utf-8");
 define(IN_CF, true);
 define(SCRIPT, 'blog');
 require  dirname(__FILE__).'/include/common.inc.php';
-$pagenow = $_GET['page'];
+if (isset($_GET['page'])){
+	$pagenow = $_GET['page'];
+	//如果page参数不存在，或者不合法，使初始页面的参数为1 
+	if (empty($_GET['page']) || $_GET['page'] < 0 || !is_numeric($_GET['page'])){
+		$pagenow = 1;
+	}else{
+		$pagenow = intval($_GET['page']);//对浮点数取整
+	}
+}else{
+	$pagenow = 1;
+}
+
 $pagesize = 20;
+
+$page_sum = mysql_num_rows(_query("SELECT g_username FROM g_user"));//返回所有搜索结果的字段数
+if ($page_sum == 0){
+	$page_abs = 1;//如果数据库中没有数据，默认显示第一页
+}else{
+	$page_abs = ceil($page_sum/$pagesize);
+}
+
+if ($pagenow > $page_abs){
+	$pagenow = $page_abs;
+}
 $pagenum = ($pagenow-1)*$pagesize;
 $result = mysql_query("SELECT g_username,g_face,g_sex FROM g_user ORDER by g_reg_time DESC LIMIT $pagenum,$pagesize");
-$page_sum = mysql_num_rows(_query("SELECT g_username FROM g_user"));//返回所有搜索结果的字段数
-$page_abs = ceil($page_sum/$pagesize);
+
 ?>
 <!DOCTYPE html>
 <html>
